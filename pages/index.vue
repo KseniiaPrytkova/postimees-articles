d9a1bc09c275d9a62f7efc2d54362863fbb24d63
Kseniia Prytkova <prytkovakseniia@gmail.com>
2021-07-19 18:34:20 +0300

    add dynamic data

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,21 @@
     <h1>Articles</h1>
 
     <ul>
-      <li v-for="article of 5" :key="article">
-          <!-- <a href="#" @click.prevent="openUser(user)">{{user.name}}</a> -->
-          Article {{article}}
+      <li v-for="article of articles" :key="article.id">
+          <a href="#" @click.prevent="openArticle(article)">{{article.headline}}</a>
+          <!-- Article {{article}} -->
       </li>
     </ul>
   </section>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    articles: []
+  }),
+  async mounted() {
+    this.articles = await this.$axios.$get('https://services.postimees.ee/rest/v1/sections/81/editorsChoice/articles?limit=5')
+  }
+}
 </script>
