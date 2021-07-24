2b7a8c6341a76c3ffb3f06106be1739215a7e4ae
Kseniia Prytkova <prytkovakseniia@gmail.com>
2021-07-24 22:00:28 +0300

    fix formatting

--- components/Jumbotron.vue
+++ components/Jumbotron.vue
@@ -57,25 +57,31 @@ export default {
             line-height: 110%;
             margin: 0% 5% 10% 2%;
         }
+
         img.center {
             width: 12vh;
         }
+
         #top-button-desctop {
             display: none;
         }
+
         #top-button-mob .btn {
             font-size: 4vw;
             font-weight: bold;
             margin: 10% 10% 5% 2%;
         }
+
         .comments-icon {
             margin: 10% 10% 10% 5%;
             font-size: 2.5vh;
         }
+
         .comments-icon img {
             width: 4vh;
         }
     }
+
     @media (min-width: 576px) and (max-width: 768px) { 
         .text-center p {
             font-size: 6vh;
@@ -85,29 +91,36 @@ export default {
         img.center {
             width: 9%;
         }
+
         #top-button-mob {
             display: none;
         }
+
         .comments-icon {
             display: none;
         }
     }
+
     @media (min-width: 768px) { 
         .text-center p {
             font-size: 6vh;
             margin: 8%;
             line-height: 120%;
         }
+
         img.center {
             width: 9%;
         }
+
         #top-button-mob {
             display: none;
         }
+
         .comments-icon {
             display: none;
         }
     }
+
     .jumbotron {
         background: url(https://images.unsplash.com/photo-1600836080410-6e9e0941a40b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80) no-repeat center;
         background-image: -moz-linear-gradient(0deg, rgb(0,0,0) 0%, rgba(0,0,0,0.18039) 100%);
@@ -118,6 +131,7 @@ export default {
         min-width: 100%;
         height: 100vh;
     }
+
     #top-button-desctop .btn {
         background-color: #c5cacd;
         color: #08b1f2;
@@ -125,10 +139,12 @@ export default {
         font-size: 14px;
         margin: 1% 0% 0% 1%;
     }
+
     img.center {
         display: block;
         margin: 0 auto;
     }
+
     .bottom-button img:hover {
         cursor: pointer;
         opacity: 0.5;
